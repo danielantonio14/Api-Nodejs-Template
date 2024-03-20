47fd2542351810a71d4d0c612fcbf74fb358cf5d
danielantonio14 <danielantonio.dad52@gmail.com>
2024-03-20 23:21:28 +0100

    add middlewares

--- storeapi/app.js
+++ storeapi/app.js
@@ -1,1 +1,19 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express= require('express');
+const app = express()
+
+
+const notFoundMiddeware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+app.use(notFoundMiddeware)
+app.use(errorHandlerMiddleware)
+
+
+
+
+
+
