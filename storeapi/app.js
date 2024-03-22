5a67d8ee737ad9b084b6801c67fd1abcec03bec0
danielantonio14 <danielantonio.dad52@gmail.com>
2024-03-22 14:01:35 +0100

    config server update

--- storeapi/app.js
+++ storeapi/app.js
@@ -1,37 +1,35 @@
 require('dotenv').config()
+
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const notMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
 
 
-const port = process.env.port || 3000;
+//middleware
+app.use(express.json())
+app.use(notMiddleware)
+app.use(errorMiddleware)
+
+
 // Rota padrão
 app.get('/', (req, res) => {
     res.send('<h1> Store API</h1><a href="#">Acess este Link para Teste </a>');
 });
 
-const notMiddleware = require('middleware/not-found');
-const errorMiddleware = require('middleware/error-handler');
-
-
 
 // Iniciando o servidor
-
-const  start = async (req, res, next)=>{
-    
+const start = async (req, res) => {
     try {
         app.listen(port, () => {
             console.log(`Servidor está rodando em http://localhost:${port}`);
         });
     }
-    
-    catch (eror) {
-        console.log(eror);
+    catch (error) {
+        console.log(err)
     }
-    
 }
-
-start()
-
 
